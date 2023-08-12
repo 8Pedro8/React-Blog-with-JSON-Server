617d15128e76bc8444e68db482f930ebbe062cdd
“8pedro8” <pedrammm@msn.com>
2023-08-12 18:13:31 +0200

    Create and add Home component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import './App.css'
 import Navbar from './Navbar';
+import Home from './Home';
 
 function App() {
 
@@ -7,7 +8,7 @@ function App() {
     <div className='App'>
       <Navbar />
       <div className="content">
-        <h1>App Component</h1>
+        <Home />
       </div>
     </div>
   )
