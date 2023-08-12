f8aa1e52b99211c59efbbb6bbb424ab1998890cd
“8pedro8” <pedrammm@msn.com>
2023-08-12 19:39:37 +0200

    Add and preview sample blog content

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,4 +1,12 @@
+import { useState } from "react";
+
 const Home = () => {
+
+    const [blogs, setBlogs] = useState([
+        { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
+        { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
+        { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
+    ])
 
     const handleClick = (name, e) => {
         console.log('hello ' + name, e.target);
@@ -6,7 +14,12 @@ const Home = () => {
 
     return ( 
         <div className="home">
-            <h2>Home Page</h2>
+            {blogs.map((blog) => (
+                <div className="blog-preview" key={blog.id}>
+                    <h2>{ blog.title }</h2>
+                    <p>Written by {blog.author}</p>
+                </div>
+            ))}
             <button onClick={(e) => handleClick('Naruto', e)}>Click me</button>
         </div>
      );
