72269a62e0f7be54f46efbab3b89a4994735f150
“8pedro8” <pedrammm@msn.com>
2023-08-12 18:59:09 +0200

    Test click event on Home Page

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,7 +1,13 @@
 const Home = () => {
+
+    const handleClick = (name, e) => {
+        console.log('hello ' + name, e.target);
+    }
+
     return ( 
         <div className="home">
             <h2>Home Page</h2>
+            <button onClick={(e) => handleClick('Naruto', e)}>Click me</button>
         </div>
      );
 }
