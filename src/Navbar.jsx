d3726be735417781d43e243506b9524ebd1da9dd
“8pedro8” <pedrammm@msn.com>
2023-08-12 18:05:36 +0200

    Create Navbar Component

---
+++ src/Navbar.jsx
@@ -0,0 +1,14 @@
+// sfc (stateless functional component)
+const Navbar = () => {
+    return ( 
+        <nav className="navbar">
+            <h1>The React Blog</h1>
+            <div className="links">
+                <a href="/">Home</a>
+                <a href="/create">New Blog</a>
+            </div>
+        </nav>
+     );
+}
+ 
+export default Navbar;
