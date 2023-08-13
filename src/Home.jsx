6c4e45abc4833a9889268ec787a5967b34c2f36e
“8pedro8” <pedrammm@msn.com>
2023-08-13 18:20:57 +0200

    Show fetch error on blog page

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,40 +1,45 @@
-import { useState, useEffect } from "react";
-import BlogList from "./BlogList";
+import { useState, useEffect } from "react"
+import BlogList from "./BlogList"
 
 const Home = () => {
 
     const [blogs, setBlogs] = useState(null)
     const [isPending, setIsPending] = useState(true)
+    const [error, setError] = useState(null)
 
     const handleDelete = (id) => {
-        const newBlogs = blogs.filter(blog => blog.id !== id);
-        setBlogs(newBlogs);
+        const newBlogs = blogs.filter(blog => blog.id !== id)
+        setBlogs(newBlogs)
     }
 
     useEffect(() => {
         setTimeout(() => { // To simulate real server request
             fetch('http://localhost:8000/blogs')
                 .then(res => {
-                    if(!res.ok) throw Error('Failed to fetch the data');
-                    return res.json();
+                    if(!res.ok) throw Error('Failed to fetch the data')
+                    return res.json()
                 })
                 .then(data => {
                     //console.log(data);
-                    setBlogs(data);
+                    setBlogs(data)
                     setIsPending(false)
+                    setError(null)
                 })
                 .catch(err => {
-                    console.log(err.message);
+                    //console.log(err.message);
+                    setIsPending(false)
+                    setError(err.message)
                 })
-        }, 1000);
-    }, []);
+        }, 1000)
+    }, [])
 
     return ( 
         <div className="home">
+            { error && <div>{ error }</div> }
             { isPending && <div>Loading...</div> }
-            {blogs && <BlogList blogs={blogs} title="All Blogs" handleDelete={handleDelete}/>}
+            { blogs && <BlogList blogs={blogs} title="All Blogs" handleDelete={handleDelete}/> }
         </div>
-     );
+     )
 }
  
 export default Home;
