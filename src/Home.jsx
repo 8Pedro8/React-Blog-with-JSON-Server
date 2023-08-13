29f7ac1427c10f2867eb41f46b808fcfc12d8767
“8pedro8” <pedrammm@msn.com>
2023-08-13 18:05:56 +0200

    Create loading msg

--- src/Home.jsx
+++ src/Home.jsx
@@ -4,6 +4,7 @@ import BlogList from "./BlogList";
 const Home = () => {
 
     const [blogs, setBlogs] = useState(null)
+    const [isPending, setIsPending] = useState(true)
 
     const handleDelete = (id) => {
         const newBlogs = blogs.filter(blog => blog.id !== id);
@@ -11,18 +12,22 @@ const Home = () => {
     }
 
     useEffect(() => {
-        fetch('http://localhost:8000/blogs')
-            .then(res => {
-                return res.json();
-            })
-            .then(data => {
-                //console.log(data);
-                setBlogs(data);
-            });
+        setTimeout(() => { // To simulate real server request
+            fetch('http://localhost:8000/blogs')
+                .then(res => {
+                    return res.json();
+                })
+                .then(data => {
+                    //console.log(data);
+                    setBlogs(data);
+                    setIsPending(false)
+                });
+        }, 1000);
     }, []);
 
     return ( 
         <div className="home">
+            { isPending && <div>Loading...</div> }
             {blogs && <BlogList blogs={blogs} title="All Blogs" handleDelete={handleDelete}/>}
         </div>
      );
